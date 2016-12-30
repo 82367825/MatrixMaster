6e387f531de51cfb4191a6d2f111877ed6a47848
linzewu <82367825@qq.com>
2016-12-30 15:44:41 +0800

    Initial commit

---
+++ app/src/main/java/com/zero/matrixmaster/MatrixActivity.java
@@ -0,0 +1,16 @@
+package com.zero.matrixmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by linzewu on 2016/12/30.
+ */
+
+public class MatrixActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
