4c730ec2ebed843efe7931b5a1ecf65763004ea6
linzewu <82367825@qq.com>
2017-01-24 16:41:35 +0800

    完善demo

--- app/src/main/java/com/zero/matrixmaster/MatrixActivity.java
+++ app/src/main/java/com/zero/matrixmaster/MatrixActivity.java
@@ -2,6 +2,7 @@ package com.zero.matrixmaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by linzewu on 2016/12/30.
@@ -13,5 +14,8 @@ public class MatrixActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
+        
+        MatrixView matrixView = (MatrixView) findViewById(R.id.matrix_view);
+        matrixView.setImageResource(R.drawable.pic);
     }
 }
--- app/src/main/java/com/zero/matrixmaster/MatrixView.java
+++ app/src/main/java/com/zero/matrixmaster/MatrixView.java
@@ -1,13 +1,11 @@
 package com.zero.matrixmaster;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 
@@ -19,42 +17,52 @@ public class MatrixView extends ImageView {
     
     public MatrixView(Context context) {
         super(context);
+        init();
     }
 
     public MatrixView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MatrixView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+    
+    private ScaleGestureDetector mScaleGestureDetector;
+    
+    private void init() {
+        mScaleGestureDetector = new ScaleGestureDetector(getContext(), new 
+                MatrixScaleGestureDetector());
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
-        }
-        
-        return super.onTouchEvent(event);
+        return mScaleGestureDetector.onTouchEvent(event);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    private class MatrixScaleGestureDetector implements android.view.ScaleGestureDetector
+            .OnScaleGestureListener {
 
-        Paint paint = new Paint();
-        Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.pic);
-        Matrix matrix = new Matrix();
-        matrix.setSkew(1, 0);
-        matrix.preScale(0.5f, 0.5f);
-        canvas.drawBitmap(bitmap, matrix, paint);
-        
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            Matrix matrix = getImageMatrix();
+            matrix.postScale(detector.getScaleFactor(), detector.getScaleFactor(), 
+                    getWidth() / 2, getHeight() / 2);
+            setImageMatrix(matrix);
+            invalidate();
+            return true;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
+
+        }
     }
 }
